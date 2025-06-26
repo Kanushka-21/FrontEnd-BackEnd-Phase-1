f40f9e20c2c8796aafa6078275581c808ec4e9ba
Kanushka-62 <iit21062@std.uwu.ac.lk>
2025-06-26 19:51:55 +0530

    Remove unused DashboardPage and GemListingForm; update SellerDashboard to use NewGemListingForm

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -5,7 +5,6 @@ import { Toaster } from 'react-hot-toast';
 // Pages
 import LoginPage from '@/pages/LoginPage';
 import RegisterPage from '@/pages/RegisterPage';
-import DashboardPage from '@/pages/DashboardPage';
 import HomePage from '@/pages/HomePage';
 import MarketplacePage from '@/pages/MarketplacePage';
 
